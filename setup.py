26bb6aa56661c5fdb0c0e3945d398b2fef16f166
Henri Hulski <henri.hulski@gazeta.pl>
2019-11-09 22:06:25 +0100

    Add integration for the Black code formatter

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 import io
 from setuptools import setup, find_packages
 
-long_description = '\n'.join((
-    io.open('README.rst', encoding='utf-8').read(),
-    io.open('CHANGES.txt', encoding='utf-8').read()
-))
+long_description = "\n".join(
+    (
+        io.open("README.rst", encoding="utf-8").read(),
+        io.open("CHANGES.txt", encoding="utf-8").read(),
+    )
+)
 
 setup(
-    name='importscan',
-    version='0.2.dev0',
-    description='Recursively import modules and sub-packages',
+    name="importscan",
+    version="0.2.dev0",
+    description="Recursively import modules and sub-packages",
     long_description=long_description,
     classifiers=[
         "Intended Audience :: Developers",
@@ -18,11 +20,11 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    keywords='decorator import package',
+    keywords="decorator import package",
     author="Martijn Faassen",
     author_email="faassen@startifact.com",
     url="http://importscan.readthedocs.io",
@@ -30,19 +32,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'setuptools'
-    ],
+    install_requires=["setuptools"],
     extras_require=dict(
-        test=[
-            'pytest >= 2.9.0',
-            'pytest-remove-stale-bytecode',
-        ],
-        coverage=[
-            'pytest-cov',
-        ],
-        pep8=[
-            'flake8',
-        ],
-    )
+        test=["pytest >= 2.9.0", "pytest-remove-stale-bytecode",],
+        coverage=["pytest-cov",],
+        pep8=["flake8", "black",],
+    ),
 )
